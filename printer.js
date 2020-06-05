4abfe0e05d86a37923ac62198a4d5a65b5969140
Jason Barr <me@jasonbarr.dev>
2020-06-05 16:21:24 -0500

    Create printer function

--- printer.js
+++ printer.js
@@ -1,1 +1,13 @@
-const pr_str = (obj) => {};
+const { _list_Q } = require("./types");
+
+const pr_str = (obj) => {
+  if (_list_Q(obj)) {
+    return `(${obj.map((e) => pr_str(e)).join(" ")})`;
+  } else if (typeof obj === "symbol") {
+    return Symbol.keyFor(obj);
+  } else if (obj === null) {
+    return "nil";
+  }
+
+  return obj.toString();
+};
